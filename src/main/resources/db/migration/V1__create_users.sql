bec994a191c9beb69d8e46f28bf622cc42c17f47
Atomic-Shadow7002 <atomic7002@gmail.com>
2026-06-21 11:11:54 +0530

    initial commit

---
+++ src/main/resources/db/migration/V1__create_users.sql
@@ -0,0 +1,13 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(20) NOT NULL,
+    enabled BOOLEAN DEFAULT TRUE,
+    email_verified BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
